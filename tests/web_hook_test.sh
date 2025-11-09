37c795536bdabfdbe0de109baaf977e56284498f
Alex <format37@gmail.com>
2025-11-10 00:41:17 +0400

    legacy documentation removed

---
+++ tests/web_hook_test.sh
@@ -0,0 +1,3 @@
+curl --request POST https://scriptlab.duckdns.org/webhook/233f4f29-4d6f-4d0d-9f53-866f4d3ba85b \
+  --header "Content-Type: application/json" \
+  --data '{"market_state":"market is very active"}'
