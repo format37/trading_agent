b8b57396fe815d1c622e8cab6dfa06e14c6b559b
Alex <format37@gmail.com>
2025-11-10 00:47:05 +0400

    a bit refactoring

--- tests/web_hook_test.sh
+++
@@ -1,3 +0,0 @@
-curl --request POST https://scriptlab.duckdns.org/webhook/233f4f29-4d6f-4d0d-9f53-866f4d3ba85b \
-  --header "Content-Type: application/json" \
-  --data '{"market_state":"market is very active"}'
